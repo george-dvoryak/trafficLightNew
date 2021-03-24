594f53de0315215f784627b042ae89bc6092a3a4
Georgy Dvoryak <gosha.dvoryak@gmail.com>
2021-03-25 00:10:25 +0300

    Initial Commit

---
+++ trafficLightNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trafficLightNew
+//
+//  Created by George Kook on 25.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
