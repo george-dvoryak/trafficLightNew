eff4448b41ef6c02ee2871d844f76fa9316863b1
Georgy Dvoryak <gosha.dvoryak@gmail.com>
2021-03-25 00:42:15 +0300

    made circles + corner radius + alpha .  a

--- trafficLightNew/ViewController.swift
+++ trafficLightNew/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var textButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        redLight.layer.cornerRadius = 55
+        redLight.alpha = 0.4
+        yellowLight.layer.cornerRadius = 55
+        yellowLight.alpha = 0.4
+        greenLight.layer.cornerRadius = 55
+        greenLight.alpha = 0.4
+        textButton.layer.cornerRadius = 15
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func textButtonPressed() {
+    }
+    
 }
 
